e844d9ea3c0f434da3aae3c96f00a98e56c72270
Gulnawaj <gulnawaj001646@gmail.com>
2026-08-22 19:16:51 +0530

    Prepare backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config' ;
+import 'dotenv/config';
 import { connectDB } from './config/db.js';
+
 import authRouter from './routes/authRoutes.js';
 import projectRouter from './routes/projectRoutes.js';
 import communityRouter from './routes/communityRoutes.js';
 import paymentRouter from './routes/paymentRoutes.js';
 
-
-const PORT = 4000;
 const app = express();
 
-//Middlewares
-
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-
+    origin: true,
+    credentials: true
 }));
-app.use(express.json({limit:"1mb"}));
 
-//DB
+app.use(express.json({ limit: "1mb" }));
+
 connectDB();
 
-//Routes
-app.use("/api/auth",authRouter);
-app.use("/api/projects",projectRouter);
-app.use("/api/community",communityRouter);
-app.use("/api/payments",paymentRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/community", communityRouter);
+app.use("/api/payments", paymentRouter);
 
-app.get('/' , (req , res) => {
+app.get('/', (req, res) => {
     res.send("API WORKING");
 });
 
-app.listen(PORT,() => {
-    console.log(`Server Started on http://localhost:${PORT}`);
-});
+export default app;
